e34bff0edfb9d0001bde8dacf065967825306727
GravityNinja <jckellum@gmail.com>
2020-03-14 13:27:27 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit  {
+  name = 'Angular Sandbox';
+  subject: Subject<boolean>;
+
+  ngOnInit(): void {
+    this.subject.next(true);
+  }
 }
